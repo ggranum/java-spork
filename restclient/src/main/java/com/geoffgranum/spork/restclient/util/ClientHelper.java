c777928656801dc781c0d9ce1bbe6932bcb65974
Geoff Granum <ggranum@users.noreply.github.com>
2020-03-06 09:08:40 -0800

    Fix(Client): Response filter should not attempt to log large responses. (#8)
    Response filter should not attempt to log large responses. Especially when logging is off.
    
    Removed CapturingClientResponseFilter.java as it is a touch dangerous for a framework to support.
    Filter is now a no-op if the Log Level will suppress the output.
    Filter now limits response size to 2kb by default, but is configurable via a system property.

--- restclient/src/main/java/com/geoffgranum/spork/restclient/util/ClientHelper.java
+++ restclient/src/main/java/com/geoffgranum/spork/restclient/util/ClientHelper.java
@@ -6,13 +6,16 @@
 
 package com.geoffgranum.spork.restclient.util;
 
-import com.geoffgranum.spork.restclient.CapturingClientResponseFilter;
+import com.geoffgranum.spork.restclient.LoggingClientResponseFilter;
 import com.geoffgranum.spork.restclient.security.AddAuthHeadersRequestFilter;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 /**
+ * Todo: Deprecate this class and make an instance based version, so consumers can subclass it and override specific methods.
+ * That said, it's easy enough to copy-paste these utilities into a custom class.
+ *
  * @author ggranum
  */
 public class ClientHelper {
@@ -23,14 +26,13 @@ public class ClientHelper {
 
   public static <T> T createResource(String baseUri, Class<T> resourceClass, String userName, String password) {
     Client client = createClient(userName, password);
-    client.register(new CapturingClientResponseFilter());
+    client.register(new LoggingClientResponseFilter());
     return proxyResource(client, baseUri, resourceClass);
   }
 
   public static Client createClient(String username, String password) {
     Client client = ClientBuilder.newClient();
     client.register(new AddAuthHeadersRequestFilter(username, password));
-
     return client;
   }
 
