e221b744f61247e3a66c761f037dc0c64b5fc348
ggranum-fsltd <geoff.granum@gmail.com>
2020-03-06 09:07:26 -0800

    Fix(Client): Response filter should not attempt to log large responses.
    Response filter should not attempt to log large responses. Especially when logging is off.
    
    Removed CapturingClientResponseFilter.java as it is a touch dangerous for a framework to support.
    Filter is now a no-op if the Log Level will suppress the output.
    Filter now limits response size to 2kb by default, but is configurable via a system property.

--- restclient/src/main/java/com/geoffgranum/spork/restclient/LoggingClientResponseFilter.java
+++ restclient/src/main/java/com/geoffgranum/spork/restclient/LoggingClientResponseFilter.java
@@ -6,9 +6,11 @@
 package com.geoffgranum.spork.restclient;
 
 import com.geoffgranum.spork.common.log.Log;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicLong;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.ClientResponseFilter;
@@ -19,14 +21,41 @@ import org.apache.commons.io.IOUtils;
  */
 public class LoggingClientResponseFilter implements ClientResponseFilter {
 
+  public static final int MAX_RESPONSE_SIZE = 2 * 1024;
+  private final AtomicLong maxResponseLogSize = new AtomicLong(0L);
+
+  /**
+   * Gets the max response log size from system properties if it has not yet been set.
+   * This method is called on each response, but the value is lazily initialized on the first response for this instance of this class.
+   * The value is initialized from the property "com.geoffgranum.spork.restclient.maxResponseLogSize", or set to MAX_RESPONSE_SIZE ( 2kb ) by default.
+   */
+  public long getMaxResponseLogSize() {
+    if(maxResponseLogSize.get() == 0) { // we don't synchronize because it's not important if we get a race condition here. Worst case is missing log message.
+      long size = MAX_RESPONSE_SIZE;
+      String key = "com.geoffgranum.spork.restclient.maxResponseLogSize";
+      try {
+        size = Long.parseLong(System.getProperty(key, "" + MAX_RESPONSE_SIZE));
+      } catch (NumberFormatException e) {
+        Log.warn(getClass(), "Could not parse max response log size property '%s' as a long value. Using default of '%s'", key, MAX_RESPONSE_SIZE);
+      }
+      maxResponseLogSize.set(size);
+    }
+    return maxResponseLogSize.get();
+  }
+
   @Override
-  public void filter(
-      ClientRequestContext requestContext,
-      ClientResponseContext responseContext) throws IOException {
-    InputStream stream = responseContext.getEntityStream();
-    String response = IOUtils.toString(stream, StandardCharsets.UTF_8.name());
-    stream.reset();
-    Log.trace(getClass(), response);
+  public void filter(ClientRequestContext requestContext, ClientResponseContext responseContext) throws IOException {
+    if(Log.traceEnabled(getClass())) {
+      InputStream stream = responseContext.getEntityStream();
+      // Stream can be null if response is empty.
+      if(stream != null) {
+        byte[] bytes = IOUtils.toByteArray(stream);
+        if(bytes.length < getMaxResponseLogSize()) {
+          responseContext.setEntityStream(new ByteArrayInputStream(bytes));
+          Log.trace(getClass(), new String(bytes, StandardCharsets.UTF_8));
+        }
+      }
+    }
   }
 }
  
